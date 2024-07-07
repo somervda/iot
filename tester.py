a49ffd6017f8d1b35df8e4cdc42ac991e5f2d533
iot <davidsomerville@comcast.net>
2024-07-07 12:37:21 -0400

    Added some exception handeling

--- tester.py
+++ tester.py
@@ -5,25 +5,29 @@ import time
 # Useful select to see recent data
 # select id,umt,to_timestamp(umt) as umtdate,data,application_id,device_id from measurement order by umt desc
 while True:
-    contents = urllib.request.urlopen("http://somerville.noip.me:37007/read?user=david").read()
-    if contents.decode('ascii')=='None':
-        print("end of data - sleeping 3 minutes...")
-        time.sleep(180)
-    else:
-        data = json.loads(contents)
-        application_id = data["appID"]
-        del data["appID"]
-        device_id = data["deviceID"]
-        del data["deviceID"]
-        umt = data["sensorTimestamp"]
-        del data["sensorTimestamp"]
-        del data["iotTimestamp"]
+    try:
+        contents = urllib.request.urlopen("http://somerville.noip.me:37007/read?user=david").read()
+        if contents.decode('ascii')=='None':
+            print("end of data - sleeping 3 minutes...")
+            time.sleep(180)
+        else:
+            data = json.loads(contents)
+            application_id = data["appID"]
+            del data["appID"]
+            device_id = data["deviceID"]
+            del data["deviceID"]
+            umt = data["sensorTimestamp"]
+            del data["sensorTimestamp"]
+            del data["iotTimestamp"]
 
-        print(data)
+            print(data)
 
-        db = Dbiot(quiet=False)
-        print(db.addMeasurement(umt,application_id,device_id,data,adjustEpoch=False))
+            db = Dbiot(quiet=False)
+            print(db.addMeasurement(umt,application_id,device_id,data,adjustEpoch=False))
 
-        # print(Dbiot.listTable("select cast(to_timestamp(umt) as date) as umtdate from measurement"))
-        db = None
-        time.sleep(1)
+            # print(Dbiot.listTable("select cast(to_timestamp(umt) as date) as umtdate from measurement"))
+            db = None
+            time.sleep(1)
+    except Exception as error:
+        print("An exception occurred:", error)
+        time.sleep(10)
