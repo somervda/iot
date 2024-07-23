43fefeaed8e1aee52c6948c1529be70b17847845
iotpi4 <david.a.somerville@outlook.com>
2024-07-23 17:16:24 -0400

    Added second sql example

--- iotLoader.py
+++ iotLoader.py
@@ -12,6 +12,21 @@ JOIN application ON measurement.application_id = application.id
 JOIN device ON measurement.device_id = device.id
 order by umt desc
 limit 10
+
+Example: Group into daily trends
+SELECT to_timestamp(date_part('epoch',date_trunc('day',to_timestamp(UMT)))) as date
+,MAX(device_id) as device_id,MAX(application_id) as application_id,
+AVG(fahrenheit) as avg_fahrenheit,
+count(umt) FROM (
+select measurement.id,umt,to_timestamp(umt) as umtdate,data,
+((data->>'celsius')::numeric * 9/5) + 32 as fahrenheit,
+device_id,application_id,application.name,device.name from measurement 
+JOIN application ON measurement.application_id = application.id
+JOIN device ON measurement.device_id = device.id
+) as foo 
+GROUP BY date,device_id 
+ORDER BY date desc
+
 """
 
 while True:
